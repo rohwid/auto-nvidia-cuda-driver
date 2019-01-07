38d67882b0fe73865e47d3c2059b78bca1f8ea9b
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2019-01-07 15:49:31 +0700

    fix .bashrc for cuda

--- 3_install_cuda.sh
+++ 3_install_cuda.sh
@@ -51,7 +51,7 @@ cudnn() {
 
     echo "[CUDA-TSFLOW] Cofiguring cuda in linux enviroment.."
     echo " " >> ~/.bashrc
-    echo "# CUDA Enviroment"
+    echo "# CUDA Enviroment" >> ~/.bashrc
     echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"' >> ~/.bashrc
     echo 'export CUDA_HOME="/usr/local/cuda"' >> ~/.bashrc
   else
