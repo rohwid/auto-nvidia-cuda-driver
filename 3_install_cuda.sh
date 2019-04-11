b6be9113800f06b7432f798adbf93f0a8332a944
Rohman Widiyanto <Rohman Widiyanto>
2019-04-11 14:08:55 +0700

    change installer directory

--- 3_install_cuda.sh
+++ 3_install_cuda.sh
@@ -2,9 +2,9 @@
 
 cuda() {
   read -n1 -r -p "Install CUDA. press ENTER to continue!" ENTER
-  if [[ -f installer/${CUDA} ]]; then
-    echo "[CUDA-TSFLOW] Installing CUDA.."
-    sudo sh installer/${CUDA} --override --silent --toolkit
+  if [[ -f installers/${CUDA} ]]; then
+    echo "[CUDA-TSFLOW] Installing $.."
+    sudo sh installers/${CUDA} --override --silent --toolkit
     echo "[CUDA-TSFLOW] CUDA Installation done."
     echo " "
 
@@ -18,10 +18,10 @@ cuda() {
       COUNTER=1
 
       while [ $COUNTER -le $NO_PATCH ]; do
-        echo 'Here the content of "installer" directory:'
-        ls installer/
+        echo 'Here the content of "installers" directory:'
+        ls installers/
         read -p "Please specify CUDA update patch no. $COUNTER: " CUPDATE
-        sudo sh installer/${CUPDATE}
+        sudo sh installers/${CUPDATE}
         let COUNTER=COUNTER+1
       done
 
@@ -32,7 +32,7 @@ cuda() {
       exit
     fi
   else
-    echo "[CUDA-TSFLOW] CUDA installer not found."
+    echo "[CUDA-TSFLOW] CUDA installers not found."
     echo "[CUDA-TSFLOW] Installation aborted"
     exit
   fi
@@ -40,13 +40,13 @@ cuda() {
 
 cudnn() {
   read -n1 -r -p "Install CUDNN. press ENTER to continue!" ENTER
-  if [[ -f installer/${CUDNN} ]]; then
+  if [[ -f installers/${CUDNN} ]]; then
     echo "[CUDA-TSFLOW] Installing CUDNN.."
 
-    mkdir installer/cudnn
-    tar -xzvf installer/${CUDNN} -C installer/cudnn --strip-components=1
-    sudo cp installer/cudnn/include/cudnn.h /usr/local/cuda/include
-    sudo cp installer/cudnn/lib64/libcudnn* /usr/local/cuda/lib64
+    mkdir installers/cudnn
+    tar -xzvf installers/${CUDNN} -C installers/cudnn --strip-components=1
+    sudo cp installers/cudnn/include/cudnn.h /usr/local/cuda/include
+    sudo cp installers/cudnn/lib64/libcudnn* /usr/local/cuda/lib64
     sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
 
     echo "[CUDA-TSFLOW] Cofiguring cuda in linux enviroment.."
@@ -70,7 +70,7 @@ cudnn() {
       exit
     fi
   else
-    echo "[CUDA-TSFLOW] CUDNN installer not found."
+    echo "[CUDA-TSFLOW] CUDNN installers not found."
     echo "[CUDA-TSFLOW] CUDNN installation failed"
     exit
   fi
@@ -78,14 +78,14 @@ cudnn() {
 
 nccl() {
   read -n1 -r -p "Install NCCL. press ENTER to continue!" ENTER
-  if [[ -f installer/${NCCL} ]]; then
+  if [[ -f installers/${NCCL} ]]; then
     echo "[CUDA-TSFLOW] Installing NCCL.."
-    mkdir installer/nccl
-    tar Jxvf installer/${NCCL} -C installer/nccl/ --strip-components=1
-    sudo cp installer/nccl/lib/libnccl* /usr/local/cuda/lib64/
-    sudo cp installer/nccl/include/nccl.h /usr/local/cuda/include/
+    mkdir installers/nccl
+    tar Jxvf installers/${NCCL} -C installers/nccl/ --strip-components=1
+    sudo cp installers/nccl/lib/libnccl* /usr/local/cuda/lib64/
+    sudo cp installers/nccl/include/nccl.h /usr/local/cuda/include/
   else
-    echo "[CUDA-TSFLOW] NCCL installer not found."
+    echo "[CUDA-TSFLOW] NCCL installers not found."
     echo "[CUDA-TSFLOW] NCCL installation failed"
     exit
   fi
@@ -93,25 +93,25 @@ nccl() {
 
 
 echo "==============================================================================================="
-echo "Welcome to Cuda Installer"
+echo "Welcome to Cuda Installers"
 echo "==============================================================================================="
 echo "WARNING! Please run this script as user not root."
 echo " "
 echo "This process will be install the latest cuda and tensorflow."
-echo 'It require "installer file" and put it in "installer directory". Here is the installer list: '
-echo " + Latest CUDA installer [ex: cuda_10.0.130_410.48_linux.run]."
+echo 'It require "installers file" and put it in "installers directory". Here is the installers list: '
+echo " + Latest CUDA installers [ex: cuda_10.0.130_410.48_linux.run]."
 echo " + Latest CUDNN library [ex: cudnn-10.0-linux-x64-v7.3.1.20.tgz]."
 echo " + Latest NCCL installer [ex: nccl_2.3.5-2+cuda10.0_x86_64.txz]."
 echo " "
-echo 'If you have the "cuda toolkit update patch" put in "installer" directory too!'
+echo 'If you have the "cuda toolkit update patch" put in "installers" directory too!'
 echo " "
 
-echo 'Have you put it all in "installer" and "installer/update-toolkit-patch" directory?'
+echo 'Have you put it all in "installers" and "installers/update-toolkit-patch" directory?'
 read -n1 -r -p "Press ENTER to continue or CTRL + C to cancel!" ENTER
 echo " "
 
-echo 'Here the content of "installer" directory:'
-ls installer/
+echo 'Here the content of "installers" directory:'
+ls installers/
 echo " "
 
 read -n1 -r -p "Check nvidia driver is installed correctly. press ENTER to continue!" ENTER
@@ -141,11 +141,11 @@ if [[ -L /usr/local/cuda ]]; then
   exit
 fi
 
-read -p "Please specify CUDA installer file. [Defaut: cuda_10.0.130_410.48_linux.run]:" CUDA
+read -p "Please specify CUDA installers file. [Defaut: cuda_10.0.130_410.48_linux.run]:" CUDA
 CUDA="${CUDA:=cuda_10.0.130_410.48_linux.run}"
-read -p "Please specify CDNN installer file. [Defaut: cudnn-10.0-linux-x64-v7.3.1.20.tgz]:" CUDNN
+read -p "Please specify CDNN installers file. [Defaut: cudnn-10.0-linux-x64-v7.3.1.20.tgz]:" CUDNN
 CUDNN="${CUDNN:=cudnn-10.0-linux-x64-v7.3.1.20.tgz}"
-read -p "Please specify NCCL installer file. [Defaut: nccl_2.3.5-2+cuda10.0_x86_64.txz]:" NCCL
+read -p "Please specify NCCL installers file. [Defaut: nccl_2.3.5-2+cuda10.0_x86_64.txz]:" NCCL
 NCCL="${NCCL:=nccl_2.3.5-2+cuda10.0_x86_64.txz}"
 echo " "
 
