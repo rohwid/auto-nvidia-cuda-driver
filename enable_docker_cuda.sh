93e5e4c50b515e2d5cb46011aebc87e826c4596f
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2022-03-10 16:57:58 +0700

    fix enable cuda

--- enable_docker_cuda.sh
+++ enable_docker_cuda.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 read -n1 -r -p "Add NVIDIA docker repository. press ENTER to continue!" ENTER
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
